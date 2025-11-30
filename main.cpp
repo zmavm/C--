0a7854bcf30b06efeb761fec629e0d104b69e1d5
GAR9 <zmavm1002@gmail.com>
2025-11-30 22:01:17 +0800

    新增 C++ 專案的基本檔案，包括 launch.json、settings.json、tasks.json 和 main.cpp，並建立可執行檔 main.exe

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    std::cout << "You are " << age << " years old." << std::endl;
+    return 0;
+}
