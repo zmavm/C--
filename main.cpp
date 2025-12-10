60a15e796b4b783f0b62a493b5006dd123e43ae9
GAR9 <zmavm1002@gmail.com>
2025-12-10 21:02:00 +0800

    loop

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main() {
-    int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    std::cout << "You are " << age << " years old." << std::endl;
-    return 0;
+    int i = true;
+    while (i){
+        int age;
+        std::cout << "Enter your age: ";
+        std::cin >> age;
+        std::cout << "You are " << age << " years old." << std::endl;
+    }
 }
